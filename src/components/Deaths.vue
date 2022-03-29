d25a41c3b1c1802e256a2ee59df57fc2cf911370
IMightBeMe <82175041+IMightBeMe@users.noreply.github.com>
2022-03-28 22:00:58 -0400

    Added the total amount of deaths

--- src/components/Deaths.vue
+++ src/components/Deaths.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="div">
-    <h2>Deaths Today</h2>
-    <p>123</p>
+    <h2>Total Deaths</h2>
+    <p>{{ data }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const data = ref(null);
+
+const fetchData = async () => {
+  const res = await fetch('https://disease.sh/v3/covid-19/all/');
+  const json = await res.json();
+
+  return json.deaths;
+};
+
+const processData = () => {
+  fetchData()
+    .then(
+      (val) =>
+        // TODO: get the data to only update if the number is different
+        (data.value = val.toLocaleString('en-US'))
+    )
+    .catch((err) => console.error(err));
+};
+
+onMounted(processData);
+</script>
+
+<style scoped></style>
