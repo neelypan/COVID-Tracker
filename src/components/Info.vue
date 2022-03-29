9e7441d0e29e72c275ba9791c784e518aa0c2858
IMightBeMe <82175041+IMightBeMe@users.noreply.github.com>
2022-03-28 22:00:30 -0400

    Put all components into this which is then passed into App.vue

---
+++ src/components/Info.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TodayCases from './TodayCases.vue';
+import TodayDeaths from './TodayDeaths.vue';
+import Cases from './Cases.vue';
+import Deaths from './Deaths.vue';
+</script>
+
+<template>
+  <div>
+    <TodayCases />
+    <TodayDeaths />
+    <Cases />
+    <Deaths />
+  </div>
+</template>
