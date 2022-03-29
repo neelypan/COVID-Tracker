2f709ed6b9ac8ed3d1671afba12a0a35b79b607f
IMightBeMe <82175041+IMightBeMe@users.noreply.github.com>
2022-03-28 22:01:39 -0400

    changed filename to incorporate total cases and deaths

---
+++ src/components/TodayCases.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="div">
+    <h2>Cases Today</h2>
+    <p>
+      {{ data }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+
+const data = ref(null);
+
+const fetchData = async () => {
+  const res = await fetch('https://disease.sh/v3/covid-19/all/');
+  const json = await res.json();
+  return json.todayCases;
+};
+const processData = async () => {
+  fetchData()
+    .then(
+      (val) =>
+        // TODO: get the data to only update if the number is different
+        (data.value = val.toLocaleString('en-US'))
+    )
+    .catch((err) => console.error(err));
+};
+setInterval(processData, 900000);
+onMounted(processData);
+</script>
+
+<style>
+.div {
+  display: inline-block;
+  margin: 6px;
+  border-radius: 6px;
+  background: rgb(235, 221, 221);
+  width: max-content;
+  height: max-content;
+}
+</style>
---
+++ src/components/TodayDeaths.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="div">
+    <h2>Deaths Today</h2>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import api from './Cases.vue';
+
+const data = ref(null);
+
+const fetchData = async () => {
+  const res = await fetch('https://disease.sh/v3/covid-19/all/');
+  const json = await res.json();
+
+  return json.todayDeaths;
+};
+
+const processData = () => {
+  fetchData().then((val) => {
+    // TODO: get the data to only update if the number is different
+    data.value = val.toLocaleString('en-US');
+  });
+};
+
+setInterval(processData, 900000);
+
+onMounted(processData);
+</script>
