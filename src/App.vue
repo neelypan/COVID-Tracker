f3ed8522f8b5c877e51873e988d94a022a1f27d7
IMightBeMe <82175041+IMightBeMe@users.noreply.github.com>
2022-03-28 22:01:14 -0400

    Refactoring and minor editing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-import Cases from './components/Cases.vue';
-import Deaths from './components/Deaths.vue';
+import Info from './components/Info.vue';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 </script>
 
 <template>
-  <h1>Covid Tracker</h1>
+  <h1>COVID Tracker</h1>
 
-  <Cases />
-  <Deaths />
+  <Info />
 </template>
 
 <style>
